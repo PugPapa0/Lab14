2a4be5893b71feea8ef498ecd02bad9b044b4b78
Travis Becker <becktrav3@icloud.com>
2026-07-27 18:48:26 -0700

    L14

--- Main.java
+++ Main.java
@@ -2,8 +2,11 @@
 // COLLABORATORS:
 // LAST MODIFIED DATE:
 
-class Main {
-  public static void main(String[] args) {
+class Main
+{
+  public static void main(String[] args)
+  {
+    // DECLARATION SECTION
     double[] co2Levels = {
             371.32, 373.45, 375.98, 377.70, 379.98,
             382.09, 384.03, 385.83, 387.64, 390.10,
@@ -13,28 +16,37 @@ class Main {
 
     int[] years = new int[20];
 
-    for (int i = 0; i < years.length; i++) {
+    // INITIALIZATION SECTION
+    for (int i = 0; i < years.length; i++)
+    {
       years[i] = 2001 + i;
     }
 
+    // OUTPUT SECTION
     System.out.println("\nData displayed for each year begins at 360 ppm.");
     System.out.println("Each additional oil drum (🛢) represents an additional 1 ppm.\n");
 
+    // Column titles of graph
     System.out.print("Year");
     UtilityBelt.printCentered(55, "CO₂ in Atmosphere (ppm)");
 
+    // Print graph
     printGraph(co2Levels, years);
 
+    // Calculate increase
     double increase = co2Levels[co2Levels.length - 1] - co2Levels[0];
 
     System.out.printf("\nFrom 2001 to 2020, the average atmospheric CO₂ levels across the globe has grown %.2f ppm.%n", increase);
   }
 
 
-  public static void printBar(double value) {
-    int drums = (int) (value - 360);
+  // Prints one bar based on CO₂ value
+  public static void printBar(double value)
+  {
+    int drums = (int)(value - 360);
 
-    for (int i = 0; i < drums; i++) {
+    for (int i = 0; i < drums; i++)
+    {
       System.out.print("🛢");
     }
 
@@ -42,8 +54,11 @@ class Main {
   }
 
 
-  public static void printGraph(double[] co2Levels, int[] years) {
-    for (int i = 0; i < co2Levels.length; i++) {
+  // Prints the entire graph
+  public static void printGraph(double[] co2Levels, int[] years)
+  {
+    for (int i = 0; i < co2Levels.length; i++)
+    {
       System.out.print(years[i] + " ");
       printBar(co2Levels[i]);
       System.out.println();
