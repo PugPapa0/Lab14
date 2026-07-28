6fb7cfd009f588018ca85376ba3007657c9ce66f
Travis Becker <becktrav3@icloud.com>
2026-07-27 19:55:20 -0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Main.java

--- Main.java
+++ Main.java
@@ -44,7 +44,7 @@ class Main {
     double growth = co2Levels[co2Levels.length - 1] - co2Levels[0];
 
     System.out.printf(
-            "From 2001 to 202, the average atmospheric CO₂ levels across the globe has grown %.2f ppm.%n",
+            "From 2001 to 2020, the average atmospheric CO₂ levels across the globe has grown %.2f ppm.%n",
             growth
     );
   }
